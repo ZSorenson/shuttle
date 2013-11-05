267959061c453bea5a00a21d85a7fdac9bc65d76
Kyle Nickel <nickelkr@gmail.com>
2013-11-05 18:30:56 -0500

    Committing initial code base

---
+++ Shuttle/src/shuttle/Shuttle.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package shuttle;
+
+/**
+ *
+ * @author Kyle
+ */
+public class Shuttle {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
